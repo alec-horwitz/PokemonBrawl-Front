bd9de7ed21b53333182395a82f7a522472c1e7c0
alec-horwitz <horwitz_alec@wheatoncollege.edu>
2018-04-16 17:54:29 -0400

    made battle screen

--- src/battle.js
+++ src/battle.js
@@ -6,9 +6,19 @@ class Battle {
 
   render() {
     return `
-    <div id="pokemon-1">
+    <div id="pokemon-1" class="pokemon-frame">
+    <h1 class="center-text" data-pokename="${this.pokemon1.name}">${this.pokemon1.name}</h1>
+    <div style="width:96px;margin:auto" data-pokename="${this.pokemon1.name}">
+    <img id="${this.pokemon1.name}-front" data-pokename="${this.pokemon1.name}" src="${this.pokemon1.frontImage}" style="display:none">
+    <img id="${this.pokemon1.name}-back" data-pokename="${this.pokemon1.name}" src="${this.pokemon1.backImage}" style="display:block">
     </div>
-    <div id="pokemon-2">
+    </div>
+    <div id="pokemon-2" class="pokemon-frame">
+    <h1 class="center-text" data-pokename="${this.pokemon2.name}">${this.pokemon2.name}</h1>
+    <div style="width:96px;margin:auto" data-pokename="${this.pokemon2.name}">
+    <img id="${this.pokemon2.name}-front" data-pokename="${this.pokemon2.name}" src="${this.pokemon2.frontImage}" style="display:block">
+    <img id="${this.pokemon2.name}-back" data-pokename="${this.pokemon2.name}" src="${this.pokemon2.backImage}" style="display:none">
+    </div>
     </div>
     `
   }
--- src/pokemon.js
+++ src/pokemon.js
@@ -11,7 +11,7 @@ let Pokemon = (function Pokemon() {
 
     render() {
       let html =
-      `<div class="pokemon-container" data-pokename="${this.name}" style="width:230px;margin:10px;background:#fecd2f;color:#2d72fc" class="pokemon-frame">
+      `<div class="pokemon-container pokemon-frame" data-pokename="${this.name}" style="width:230px;margin:10px;background:#fecd2f;color:#2d72fc">
       <h1 class="center-text" data-pokename="${this.name}">${this.name}</h1>
       <div style="width:96px;margin:auto" data-pokename="${this.name}">
       <img id="${this.name}-front" data-pokename="${this.name}" src="${this.frontImage}" style="display:block">
